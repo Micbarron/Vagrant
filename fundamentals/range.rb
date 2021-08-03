684a02aa1ec82bd8f6e9e370aa7d029ae24a162c
Michael Barron <michael.barron08@gmail.com>
2021-08-02 21:50:12 -0500

    commit range

---
+++ fundamentals/range.rb
@@ -0,0 +1,10 @@
+x = (1..50)
+
+
+puts "Class Name: #{x.class}"
+
+puts"It does include 47!" if x.include? 47
+
+puts "The last number of this range is " + x.last.to_s
+puts "The max number of this range is " + x.max.to_s
+puts "The Minimun number in this range is " +x.min.to_s
