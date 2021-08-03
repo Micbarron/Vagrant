889562df461977df665db35b4e874b85910229bf
Michael Barron <michael.barron08@gmail.com>
2021-08-03 14:54:53 -0500

    commit

---
+++ fundamentals/basic13.rb
@@ -0,0 +1,30 @@
+# 1 print 1-255
+
+# (1..255).each {|n| print n, ' ' }
+
+# 2 print odd 1-255
+
+# a = (1..255)
+# puts a.select(&:odd?)
+
+# 3 print sum
+# total = 0
+# for i in 0..255
+#     total += i
+#     puts "New Number: #{i} Sum: #{total}"
+# end
+
+# 4 Iterating through an array
+
+# x = [1,3,5,7,9,13]
+# x.each {|n| print n, ' '}
+
+# 5 Find Max
+
+# x = [45, -5, -7, 99]
+# puts x.max
+
+# 6 Get Average
+ary = [2, 10, 3]
+puts mean = ary.sum(0.2) / ary.length
+
