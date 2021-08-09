94f8a98845172adb4daffeafe352120271d84fd9
Michael Barron <michael.barron08@gmail.com>
2021-08-09 13:39:05 -0500

    blogs_2_poly

--- modelPractice/blogs.rb/blogs/app/models/message.rb
+++ modelPractice/blogs.rb/blogs/app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ActiveRecord::Base
+  belongs_to :user
   belongs_to :post
+  has_many :comments, as: :commentable
   validates :author, :message, presence: true
   validates :message, length: {minimum: 15 }
 end
--- modelPractice/blogs.rb/blogs/app/models/post.rb
+++ modelPractice/blogs.rb/blogs/app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :messages
+  belongs_to :user
   belongs_to :blog
+  has_many :comments, as: :commentable
   validates :title, :content, presence: true
   validates :title, length {minimum: 7}
 end
