f2fb62ad08b80d88ac337b0572369993145c4bf2
Michael Barron <michael.barron08@gmail.com>
2021-08-15 01:11:25 -0500

    dd3 commit

--- modelPractice/dojo_dash/app/controllers/dojos_controller.rb
+++ modelPractice/dojo_dash/app/controllers/dojos_controller.rb
@@ -16,6 +16,29 @@ class DojosController < ApplicationController
             redirect_to :back
         end
     end
+
+    def show
+        @dojo = Dojo.find(params[:id])
+    end
+    def edit 
+        @dojo = Dojo.find(params[:id])
+    end
+
+    def update 
+        @dojo = Dojo.find(params[:id])
+
+        if @dojo.update(dojo_params)
+            redirect_to root_path, notice: "You have updated a Dojo!"
+        else
+            flash[:errors] = @dojo.errors.full_messages
+            redirect_to :back
+        end
+    end
+
+    def destroy
+        Dojo.find(params[:id]).destroy
+        redirect_to :root 
+    end
     
     private
         def dojo_params
