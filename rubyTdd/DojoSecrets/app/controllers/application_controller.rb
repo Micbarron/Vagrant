011eaab0d4cd123332eb9e5d933799c31bf4fcfb
Michael Barron <michael.barron08@gmail.com>
2021-08-18 22:35:44 -0500

    dojo secrets final

--- rubyTdd/DojoSecrets/app/controllers/application_controller.rb
+++ rubyTdd/DojoSecrets/app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  def current_user
-    User.find(session[:user_id]) if session[:user_id]
+  before_action :require_login
+  
+  def require_login
+    redirect_to "/sessions/new" unless session[:user_id]
   end
+  
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    else
+      nil
+    end
+  end
+
   helper_method :current_user
   protect_from_forgery with: :exception
 end
--- rubyTdd/DojoSecrets/app/controllers/users_controller.rb
+++ rubyTdd/DojoSecrets/app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :show, :update, :destroy]
   def new
   end
 
   def create
-    redirect_to '/sessions/new'
+    @user = User.create (user_params)
+    if @user.valid?    
+      redirect_to '/sessions/new'
+    else flash[:errors] = @user.errors.full_messages 
+      redirect_to :back
+    end
   end
 
   def show
@@ -11,5 +18,37 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update user_params
+      redirect_to "/users/#{@user.id}"
+    else
+      flash[:errors] = @user.errors.full_messages     
+      redirect_to "/users/#{@user.id}/edit"
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      reset_session
+      redirect_to "/users/new"
+    else
+      redirect_to "/users/#{@user.id}/edit"
+    end
+  end  
+
+  private
+  def require_correct_user
+    if current_user != User.find(params[:id])
+      redirect_to "/users/#{session[:user_id]}"
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
